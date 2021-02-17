73f498a14d21f148d7cc5c0484df46e7f836ed26
Cassandra Villegas <cchlinka@gmail.com>
2021-02-17 09:41:12 -0500

    Chapter 8 practice

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,19 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+const unorderedList = `
+    <li>fire</li>
+    <li>earth</li>
+    <li>water</li>
+    <li>air</li>
+`;
+
+console.log(unorderedList);
+
+const createList = function (unorderedList) {
+  let main = document.querySelector("main");
+  main.innerHTML = unorderedList;
+};
+
+createList(unorderedList);
